e85d2616039a1fea06340990f9be95b38e314ac4
Gaurav Sarma <gauravsarma1992@gmail.com>
2025-03-15 00:18:29 +0800

    Fetch action ID in proper format

--- cronui/src/services/action.service.ts
+++ cronui/src/services/action.service.ts
@@ -11,34 +11,34 @@ export interface Action {
 }
 
 export interface ActionResponse {
-  data: Action;
+  action: Action;
   message: string;
 }
 
 export interface ActionsResponse {
-  data: Action[];
+  actions: Action[];
   message: string;
 }
 
 class ActionService {
   async getActions(): Promise<Action[]> {
     const response = await api.get<ActionsResponse>('/actions');
-    return response.data.data;
+    return response.data.actions;
   }
 
   async getAction(id: number): Promise<Action> {
     const response = await api.get<ActionResponse>(`/actions/${id}`);
-    return response.data.data;
+    return response.data.action;
   }
 
   async createAction(action: Partial<Action>): Promise<Action> {
     const response = await api.post<ActionResponse>('/actions', action);
-    return response.data.data;
+    return response.data.action;
   }
 
   async updateAction(id: number, action: Partial<Action>): Promise<Action> {
     const response = await api.put<ActionResponse>(`/actions/${id}`, action);
-    return response.data.data;
+    return response.data.action;
   }
 
   async deleteAction(id: number): Promise<void> {
